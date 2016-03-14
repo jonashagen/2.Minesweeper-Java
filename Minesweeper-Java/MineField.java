4590f8183947589b17d49639843eb626b72a3a94
jonashagen <j_hagen88@hotmail.com>
2016-03-14 22:25:17 +0100

    Improved code
    code after improvement

--- Minesweeper-Java/MineField.java
+++ Minesweeper-Java/MineField.java
@@ -1,164 +1,170 @@
+
 import java.util.Random;
 
-class MineField{
+class MineField {
 
-	private boolean[][] mines,visible;
-	private boolean boom;
-	private final int rowMax = 5;
-	private final int colMax = 10;
-	
-	MineField(){
-		
-		mines=new boolean[rowMax][colMax];
-		visible=new boolean[rowMax][colMax];
-		boom=false;
-		
-		for(int row=0;row<rowMax;row++){
-			for(int col=0;col<colMax;col++){
-				mines[row][col]=false;
-				visible[row][col]=false;
-			}
-		}
-		
-		int counter2=15;
-		int randomRow,randomCol;
-		Random RGenerator=new Random();
-		
-		while(counter2>0){
-			
-			randomRow=Math.abs(RGenerator.nextInt()%rowMax);
-			randomCol=Math.abs(RGenerator.nextInt()%colMax);
-			
-			if(trymove(randomRow,randomCol)){
-				counter2--;
-			}
-		}
-	}	
-	private boolean trymove(int randomRow, int randomCol) {
-		if(mines[randomRow][randomCol]){
-			return false;
-		}
-		else{
-			mines[randomRow][randomCol]=true;
-			return true;
-		}
-	}
-	private void boom() {
-		for(int row=0;row<rowMax;row++){
-			for(int col=0;col<colMax;col++){
-				if(mines[row][col]){
-					visible[row][col]=true;
-				}
-			}
-		}
-		boom=true;
-		show();
-		
-		
-	}
+    private boolean[][] mines, visible;
+    private boolean boom;
+    private final int rowMax = 5;
+    private final int colMax = 10;
 
+    /**
+     * Constructs minefield.
+     */
+    MineField() {
 
-	private char drawChar(int row, int col) {
-		int count=0;
-		if(visible[row][col]){
-			if(mines[row][col]) return '*';
-			for(int irow=row-1;irow<=row+1;irow++){
-				for(int icol=col-1;icol<=col+1;icol++){
-					if(icol>=0&&icol<colMax&&irow>=0&&irow<rowMax){
-						if(mines[irow][icol]) count++;
-					}
-				}
-			}
-		}
-		else{
-			if(boom){
-				return '-';
-			}
-			{
-				
-				
-				return '?';
-			}
-		}
-		switch(count){
-		case 0:return '0';
-		case 1:return '1';
-		case 2:return '2';
-		case 3:return '3';
-		case 4:return '4';
-		case 5:return '5';
-		case 6:return '6';
-		case 7:return '7';
-		case 8:return '8';
-		
-		
-		default:return 'X';
-		}
-	}
-	public boolean getBoom(){
-		
-		return boom;
-	}
+        mines = new boolean[rowMax][colMax];
+        visible = new boolean[rowMax][colMax];
+        boom = false;
 
+        initMap();
 
-	public boolean legalMoveString(String input) {
-		String[] separated=input.split(" ");
-		int row;
-		int col;
-		try{
-			
-			
-			row=Integer.parseInt(separated[0]);
-			col=Integer.parseInt(separated[1]);
-			if(row<0||col<0||row>=rowMax||col>=colMax){
-				throw new java.io.IOException();
-			}
-		}
-		catch(Exception e){
-			System.out.println("\nInvalid Input!");
-			return false;
-		}
-		
-		if(legalMoveValue(row,col)){
-			return true;
-			
-			
-		}
-		else{
-			return false;
-		}
-	}
+        int counter2 = 15;
+        int randomRow, randomCol;
+        Random RGenerator = new Random();
 
+        while (counter2 > 0) {
+            randomRow = Math.abs(RGenerator.nextInt() % rowMax);
+            randomCol = Math.abs(RGenerator.nextInt() % colMax);
 
-	private boolean legalMoveValue(int row, int col) {
-		
-		if(visible[row][col]){
-			System.out.println("You stepped in allready revealed area!");
-			return false;
-		}
-		else{
-			visible[row][col]=true;
-		}
-		
-		if(mines[row][col]){
-			boom();
-			return false;
-		}
-		
-		return true;
-	}
-	public void show() {
-		System.out.println("\n    0 1 2 3 4 5 6 7 8 9 ");
-		System.out.println("   ---------------------");
-		for(int row=0;row<rowMax;row++){
-			System.out.print(row+" |");
-			for(int col=0;col<colMax;col++){
-				System.out.print(" "+drawChar(row,col));
-				
-			}
-			System.out.println(" |");
-		}
-		System.out.println("   ---------------------");
-	}
-	
+            if (trymove(randomRow, randomCol)) counter2--;
+        }
+    }
+    
+    /**
+     * Initializing maps.
+     */
+    private void initMap() {
+        for (int row = 0; row < rowMax; row++) {
+            for (int col = 0; col < colMax; col++) {
+                mines[row][col] = false;
+                visible[row][col] = false;
+            }
+        }
+    }
+    
+    /**
+     * @param randomRow
+     * @param randomCol
+     */
+    private boolean trymove(int randomRow, int randomCol) {
+        if (mines[randomRow][randomCol]) return false;
+        mines[randomRow][randomCol] = true;
+        return true;
+    }
+    
+    /**
+     * Checks if coordinates contains mine.
+     */
+    private void boom() {
+        for (int row = 0; row < rowMax; row++) {
+            for (int col = 0; col < colMax; col++) {
+                if (mines[row][col]) visible[row][col] = true;
+            }
+        }
+        boom = true;
+        show();
+    }
+    
+    /**
+     * Returns a char 0-8 if row and col is visible and is not a mine.
+     * @param row
+     * @param col
+     * @return char
+     */
+    private char drawChar(int row, int col) {
+        if(!visible[row][col]){
+            if(boom) return '-';
+            return '?';
+        }   
+        if(mines[row][col]) return '*';
+        
+        return Integer.toString(checkSurroundingFields(row, col)).charAt(0);
+    }
+    
+    /**
+     * Counts the number of mines surrounding the coordinates that were entered.
+     * @param row
+     * @param col
+     * @return count
+     */
+    public int checkSurroundingFields(int row, int col){
+        int count = 0;
+        for(int irow=row-1;irow<=row+1;irow++){
+            for(int icol=col-1;icol<=col+1;icol++){
+                if(icol>=0&&icol<colMax&&irow>=0&&irow<rowMax){
+                    if(mines[irow][icol]) count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    /**
+     * @return boom
+     */
+    public boolean getBoom() {
+        return boom;
+    }
+    
+    /**
+     * Checks if you made a legal move.
+     * @param input
+     * @return true/false
+     */
+    public boolean legalMoveString(String input) {
+        String[] separated = input.split(" ");
+        int row;
+        int col;
+        try {
+            row = Integer.parseInt(separated[0]);
+            col = Integer.parseInt(separated[1]);
+            if (row < 0 || col < 0 || row >= rowMax || col >= colMax) {
+                throw new java.io.IOException();
+            }
+        } catch (Exception e) {
+            System.out.println("\nInvalid Input!");
+            return false;
+        }
+        if (legalMoveValue(row, col)) return true;
+        return false;
+    }
+    
+    /**
+     * Checks if you made a legal move.
+     * @param row
+     * @param col
+     * @return true/false
+     */
+    private boolean legalMoveValue(int row, int col) {
+
+        if (visible[row][col]) {
+            System.out.println("You stepped in allready revealed area!");
+            return false;
+        }
+        visible[row][col] = true;
+
+        if (mines[row][col]) {
+            boom();
+            return false;
+        }
+        return true;
+    }
+    
+    /**
+     * Writes the minefield to screen.
+     */
+    public void show() {
+        System.out.println("\n    0 1 2 3 4 5 6 7 8 9 ");
+        System.out.println("   ---------------------");
+        for (int row = 0; row < rowMax; row++) {
+            System.out.print(row + " |");
+            for (int col = 0; col < colMax; col++) {
+                System.out.print(" " + drawChar(row, col));
+            }
+            System.out.println(" |");
+        }
+        System.out.println("   ---------------------");
+    }
+
 }
--- Minesweeper-Java/Ranking.java
+++ Minesweeper-Java/Ranking.java
@@ -1,76 +1,84 @@
+
 import java.util.Scanner;
 
-public class Ranking{
+public class Ranking {
 
-	private final int MAX_PEOPLE_LIMIT=5;
-	private String[] name;
-	private int[] record;
-	private int last;
-	
-	Ranking(){
-		name=new String[MAX_PEOPLE_LIMIT];
-		record=new int[MAX_PEOPLE_LIMIT];
-		
-		last=0;
-	}
+    private final int MAX_PEOPLE_LIMIT = 5;
+    private String[] name;
+    private int[] record;
+    private int last;
 
+    /**
+     * Makes two arrays for storing names and scores.
+     */
+    Ranking() {
+        name = new String[MAX_PEOPLE_LIMIT];
+        record = new int[MAX_PEOPLE_LIMIT];
 
-	public void recordName(int result) {
-		System.out.print("\n Please enter your name -");
-		Scanner in=new Scanner(System.in);
-		String newName=in.nextLine();
-		if((last==MAX_PEOPLE_LIMIT)&&record[MAX_PEOPLE_LIMIT-1]>result){
-			System.out.println("\nSorry you cannot enter top "+(MAX_PEOPLE_LIMIT)+" players");
-			return;
-		}
-		else if(last==MAX_PEOPLE_LIMIT){
-			name[MAX_PEOPLE_LIMIT-1]=newName;{
-				record[MAX_PEOPLE_LIMIT-1]=result;
-			}
-		}
-		else{
-			name[last]=newName;{
-				record[last]=result;{
-					last++;
-				}
-			}
-		}
-		
-		sort();
-		show();
-	}
+        last = 0;
+    }
 
+    /**
+     * Takes a name as input and stores it in the ranking list.
+     * @param result
+     */
+    public void recordName(int result) {
+        System.out.print("\n Please enter your name -");
+        Scanner in = new Scanner(System.in);
+        String newName = in.nextLine();
+        
+        if ((last == MAX_PEOPLE_LIMIT) && record[MAX_PEOPLE_LIMIT - 1] > result) {
+            System.out.println("\nSorry you cannot enter top " + (MAX_PEOPLE_LIMIT) + " players");
+            return;
+        } 
+        else if (last == MAX_PEOPLE_LIMIT) {
+            name[MAX_PEOPLE_LIMIT - 1] = newName;
+            record[MAX_PEOPLE_LIMIT - 1] = result;
+        } 
+        else {
+            name[last] = newName;
+            record[last] = result;
+            last++;
+        }
+        sort();
+        show();
+    }
 
-	public void show() {
-		if(last==0){
-			System.out.println("Still no results");
-			return;
-		}
-		System.out.println("N Name\t\tresult");
-		for(int i=0;i<last;i++){
-			System.out.println((i+1)+" "+name[i]+"\t"+record[i]);
-		}
-	}
-	
-	
-	private void sort(){
-		if(last<2) return;
-		boolean unsorted=true;
-		while(unsorted){
-			unsorted=false;
-			for(int i=0;i<(last-1);i++){
-				if(record[i+1]>record[i]){
-					int swapR=record[i];{
-						record[i]=record[i+1];{
-							record[i+1]=swapR;
-							String swapN=name[i];
-							name[i]=name[i+1];
-							name[i+1]=swapN;
-							unsorted=true;
-						}
-					}
-				}
-			}
-		}
-	}
+    /**
+     * Writes the ranking list to the screen.
+     */
+    public void show() {
+        if (last == 0) {
+            System.out.println("Still no results");
+            return;
+        }
+        System.out.println("N Name\t\tresult");
+        for (int i = 0; i < last; i++) {
+            System.out.println((i + 1) + " " + name[i] + "\t" + record[i]);
+        }
+    }
+    
+    /**
+     * Sorts the ranking list.
+     */
+    private void sort() {
+        if (last < 2) return;
+        
+        boolean unsorted = true;
+        
+        while (unsorted) {
+            unsorted = false;
+            for (int i = 0; i < (last - 1); i++) {
+                if (record[i + 1] > record[i]) {
+                    int swapR = record[i];
+                    record[i] = record[i + 1];
+                    record[i + 1] = swapR;
+                    String swapN = name[i];
+                    name[i] = name[i + 1];
+                    name[i + 1] = swapN;
+                    unsorted = true;
+                }
+            }
+        }
+    }
 }
